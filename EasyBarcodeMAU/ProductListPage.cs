ec91190d78ac2bbd4c2069b5c723124b722a603b
Orhun Yusuf Babaoglu <74192702+morpheusgodhun@users.noreply.github.com>
2023-09-29 10:55:07 +0300

    Dosya dizin düzenleme icon-

--- EasyBarcodeMAU/ProductListPage.cs
+++ EasyBarcodeMAU/ProductListPage.cs
@@ -7,7 +7,7 @@ public class ProductListPage : ContentPage
 		Content = new VerticalStackLayout
 		{
 			Children = {
-				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Orhun Test"
 				}
 			}
 		};
