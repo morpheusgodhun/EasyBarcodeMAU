35c700a4b0c041defe92af94f1d9e6b04ca71032
Orhun Yusuf Babaoglu <74192702+morpheusgodhun@users.noreply.github.com>
2023-10-03 15:16:59 +0300

    barkod ekranı giriş yapıldı api level statement değiştirildi, kütüphanede kamera izni verili olsa dahi düzenleme yapılmadan çalışmıyordu düzeltildi, anlık olarak seçimi onayla butonuna tıklandığında ilgili barkod sayfasına yönlendirme yapılıyor, alinin doldurduğu sayfadaki veriler ile veriler Binding ile propertychanged yapılacak barkod için testler 6.0, 7.0 yapıldı iki tarafta da projeye devam ediliyor barkod kütüphanesi eklendi ViewCamera eklendi **to do, problem => barkod formatı 128 olmasına karşın kod okunup value yazdırma işlevi inactive okuyup yazmıyor araştırılacak

--- EasyBarcodeMAU/MauiProgram.cs
+++ EasyBarcodeMAU/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using ZXing.Net.Maui.Controls;
 
 namespace EasyBarcodeMAU {
     public static class MauiProgram {
@@ -6,6 +7,7 @@ namespace EasyBarcodeMAU {
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseBarcodeReader()
                 .ConfigureFonts(fonts => {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
--- EasyBarcodeMAU/ScanBarcodeScreen.xaml.cs
+++ EasyBarcodeMAU/ScanBarcodeScreen.xaml.cs
@@ -1,9 +1,25 @@
+using ZXing.Net.Maui.Controls;
 namespace EasyBarcodeMAU;
 
-public partial class ScanBarcodeScreen : ContentPage
-{
-	public ScanBarcodeScreen()
-	{
-		InitializeComponent();
-	}
+public partial class ScanBarcodeScreen : ContentPage {
+    public ScanBarcodeScreen() {
+        InitializeComponent();
+        barCodereader.Options = new ZXing.Net.Maui.BarcodeReaderOptions {
+            Formats = ZXing.Net.Maui.BarcodeFormat.Ean13
+        };
+    }
+    public long BarcodeValue { get; set; }
+
+    private void barCodereader_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e) {
+        barCodereader.IsDetecting = false;
+        if (e.Results.Any()) {
+            var result = e.Results.FirstOrDefault();
+            Dispatcher.Dispatch(async () => {
+                var navigationParam = new Dictionary<string, object>() {
+                    {"qrCodeResult" , result.Value }
+                };
+                await Shell.Current.GoToAsync("..", navigationParam);
+            });
+        }
+    }
 }
