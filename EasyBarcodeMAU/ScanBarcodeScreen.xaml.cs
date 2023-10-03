fe04acb0be14299649797efda6a6155a31a2da0f
Orhun Yusuf Babaoglu <74192702+morpheusgodhun@users.noreply.github.com>
2023-10-03 15:39:12 +0300

    ScanBarcode düzgün çalışır duruma getirildi, şu anlık seçimi onayla'nın içinde direkt scan ekranı açılıyor alinin değişikliklerden sonra karar verip düzenlenecek

--- EasyBarcodeMAU/ScanBarcodeScreen.xaml.cs
+++ EasyBarcodeMAU/ScanBarcodeScreen.xaml.cs
@@ -5,7 +5,13 @@ public partial class ScanBarcodeScreen : ContentPage {
     public ScanBarcodeScreen() {
         InitializeComponent();
         barCodereader.Options = new ZXing.Net.Maui.BarcodeReaderOptions {
-            Formats = ZXing.Net.Maui.BarcodeFormat.Ean13
+            Formats = ZXing.Net.Maui.BarcodeFormat.Ean13 | ZXing.Net.Maui.BarcodeFormat.Ean8
+            | ZXing.Net.Maui.BarcodeFormat.DataMatrix | ZXing.Net.Maui.BarcodeFormat.Aztec | ZXing.Net.Maui.BarcodeFormat.Codabar
+            | ZXing.Net.Maui.BarcodeFormat.Code39 | ZXing.Net.Maui.BarcodeFormat.Code93 | ZXing.Net.Maui.BarcodeFormat.Imb
+            | ZXing.Net.Maui.BarcodeFormat.Itf | ZXing.Net.Maui.BarcodeFormat.MaxiCode | ZXing.Net.Maui.BarcodeFormat.Msi
+            | ZXing.Net.Maui.BarcodeFormat.PharmaCode | ZXing.Net.Maui.BarcodeFormat.Plessey | ZXing.Net.Maui.BarcodeFormat.QrCode
+            | ZXing.Net.Maui.BarcodeFormat.Rss14 | ZXing.Net.Maui.BarcodeFormat.RssExpanded | ZXing.Net.Maui.BarcodeFormat.UpcA
+            | ZXing.Net.Maui.BarcodeFormat.UpcE | ZXing.Net.Maui.BarcodeFormat.UpcEanExtension
         };
     }
     public long BarcodeValue { get; set; }
