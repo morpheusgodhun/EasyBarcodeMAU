dab7a3699396a2c9a4203cd914295654326fd3a1
Orhun Yusuf Babaoglu <74192702+morpheusgodhun@users.noreply.github.com>
2023-10-04 10:15:04 +0300

    ExitButton sebep olduğu hata giderildi. Splash Screen için istlog olarak => oval biçimli değiştirildi.

--- EasyBarcodeMAU/ProductListePage.xaml.cs
+++ EasyBarcodeMAU/ProductListePage.xaml.cs
@@ -5,19 +5,16 @@ namespace EasyBarcodeMAU {
         public ProductListPage() {
             InitializeComponent();
 
-            // ProductModel sýnýfýndan verileri alýn
             var productModel = new ProductModel();
             productItems = productModel.MyItems;
 
-            // Tüm ürünleri birleþtirip ListView'a baðlayýn
             var itemDescriptions = new List<string>();
             foreach (var item in productItems) {
                 var description =
                     $"ÜRÜN NUMARASI={item.Id}, Defter Numarasý={item.DefterNo}, Depo Giriþ Tarihi={item.DepoGirisTarih:dd.MM.yyyy}, Müþteri Adý ={item.MusteriAd}, Ürün Adedi ={item.UrunAdet}, Ürün Aðýrlýðý={item.UrunAgirlik}, Depo Konumu={item.DepoKonum}";
                 itemDescriptions.Add(description);
             }
-
-            //ItemList.ItemsSource = itemDescriptions;
+      
         }
 
         async void Frame_Tapped(object sender, EventArgs e)
@@ -39,7 +36,7 @@ namespace EasyBarcodeMAU {
                         string message = string.Join("\n", labelContents);
                         await DisplayAlert("Label Ýçerikleri", message, "Tamam");
                     }
-                    Navigation.PushAsync(new ScanBarcodeScreen());
+                   await Navigation.PushModalAsync(new ScanBarcodeScreen());
                 }
             }
         }
