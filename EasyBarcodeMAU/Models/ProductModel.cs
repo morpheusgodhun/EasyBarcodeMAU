b5d7c538fcb7ac16c7da88d446cdb70d52d19b60
Orhun Yusuf Babaoglu <74192702+morpheusgodhun@users.noreply.github.com>
2023-10-04 14:08:57 +0300

    eksik prop eklendi.

--- EasyBarcodeMAU/Models/ProductModel.cs
+++ EasyBarcodeMAU/Models/ProductModel.cs
@@ -29,6 +29,18 @@ namespace EasyBarcodeMAU {
                 }
             }
         }
+        private int _firmaAd;
+        public int FirmaAd {
+            get { return _firmaAd; }
+            set {
+                if (_firmaAd != value) {
+                    _firmaAd = value;
+                    OnPropertyChanged(nameof(FirmaAd));
+                }
+            }
+        }
+
+
         private int _defterNo;
         public int DefterNo {
             get { return _defterNo; }
