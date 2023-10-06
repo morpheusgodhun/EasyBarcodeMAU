78d2955d086c082e7209521ce0883f1c11b7a981
Orhun Yusuf Babaoglu <74192702+morpheusgodhun@users.noreply.github.com>
2023-10-06 12:05:27 +0300

    Viewmodel added, design

--- EasyBarcodeMAU/Models/ProductModel.cs
+++ EasyBarcodeMAU/Models/ProductModel.cs
@@ -8,8 +8,8 @@
             new ProductItemBase { Id = 2, DefterNo = 451321512, DepoGirisi = DateTime.Now, MusteriAd = "Penta Teknoloji", UrunCins = "Ekran Kartı", UrunAdet = 500, UrunAgirlik = 5000 , DepoKonum = "Bursa" },
             new ProductItemBase { Id = 3, DefterNo = 561321512, DepoGirisi = DateTime.Now, MusteriAd = "Vatan Computer", UrunCins = "Mouse", UrunAdet = 600, UrunAgirlik = 300 , DepoKonum = "İzmir" },
             new ProductItemBase { Id = 4, DefterNo = 561321512, DepoGirisi = DateTime.Now, MusteriAd = "Teknosa", UrunCins = "Laptop", UrunAdet = 600, UrunAgirlik = 9000 , DepoKonum = "Erzurum" },
-            new ProductItemBase { Id = 5, DefterNo = 671321512, DepoGirisi = DateTime.Now, MusteriAd = "ARMUT", UrunCins = "Kitap", UrunAdet = 300, UrunAgirlik = 250 , DepoKonum = "Malatya" },
-            new ProductItemBase { Id = 6, DefterNo = 781321512, DepoGirisi = DateTime.Now, MusteriAd = "Hepsiburada", UrunCins = "Kitap", UrunAdet = 1000, UrunAgirlik = 250 , DepoKonum = "Malatya" },
-            new ProductItemBase { Id = 7, DefterNo = 891321512, DepoGirisi = DateTime.Now, MusteriAd = "Getir", UrunCins = "Kulaklık", UrunAdet = 400, UrunAgirlik = 2000 , DepoKonum = "Malatya" },
+            new ProductItemBase { Id = 5, DefterNo = 671321512, DepoGirisi = DateTime.Now, MusteriAd = "ARMUT", UrunCins = "Kitap", UrunAdet = 300, UrunAgirlik = 250 , DepoKonum = "Isparta" },
+            new ProductItemBase { Id = 6, DefterNo = 781321512, DepoGirisi = DateTime.Now, MusteriAd = "Hepsiburada", UrunCins = "Kitap", UrunAdet = 1000, UrunAgirlik = 250 , DepoKonum = "Erzincan" },
+            new ProductItemBase { Id = 7, DefterNo = 891321512, DepoGirisi = DateTime.Now, MusteriAd = "Getir", UrunCins = "Kulaklık", UrunAdet = 400, UrunAgirlik = 2000 , DepoKonum = "Muğla" },
         };
     }
---
+++ EasyBarcodeMAU/ScanBarcodeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EasyBarcodeMAU.Models {
+    public class ScanBarcodeViewModel : INotifyPropertyChanged {
+        private string _barcodeValue;
+
+        public string BarcodeValue {
+            get { return _barcodeValue; }
+            set {
+                if (_barcodeValue != value) {
+                    _barcodeValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
