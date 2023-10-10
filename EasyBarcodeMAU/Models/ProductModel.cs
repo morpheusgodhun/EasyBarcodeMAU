32ef4fa4ed583ac456ccdd5e98c5e943b2ee3bd2
Ali Çukur <alicukur.yzlm@gmail.com>
2023-10-10 16:58:09 +0300

    -

--- EasyBarcodeMAU/Models/ProductModel.cs
+++ EasyBarcodeMAU/Models/ProductModel.cs
@@ -1,16 +1,17 @@
 ﻿namespace EasyBarcodeMAU.Models;
-public class ProductModel : BaseViewModel {
+public class ProductModel : BaseViewModel
+{
 
     #region List
     public List<ProductItemBase> ProductItems { get; set; } = new List<ProductItemBase>
     {
-            new ProductItemBase { Id = 1, DefterNo = 123321456, DepoGirisi = DateTime.Now, MusteriAd = "Beko", UrunCins = "Mobilya", UrunAdet = 7839858, UrunAgirlik = 195 , DepoKonum = "Yakacık Yeni Mah Veysel Sokak NO:9, Kartal/İstanbul", RequiredCount=5 },
-            new ProductItemBase { Id = 2, DefterNo = 213213123, DepoGirisi = DateTime.Now, MusteriAd = "Penta Teknoloji", UrunCins = "Ekran Kartı", UrunAdet = 3243243, UrunAgirlik = 250 , DepoKonum = "Kaynarca Mahallesi Sağduyu Sokak 2/A, Bursa/Merkez",RequiredCount=10},
-            new ProductItemBase { Id = 3, DefterNo = 453223323, DepoGirisi = DateTime.Now, MusteriAd = "Vatan Computer", UrunCins = "MousePad", UrunAdet = 4829424, UrunAgirlik = 300 , DepoKonum = "Gazi Blv. No:97, İzmir/Merkez", RequiredCount=20},
-            new ProductItemBase { Id = 4, DefterNo = 234142323, DepoGirisi = DateTime.Now, MusteriAd = "Teknosa", UrunCins = "Laptop", UrunAdet = 2935643, UrunAgirlik = 500 , DepoKonum = "Solakzade Mah. Yavuz Sultan Selim Bulvarı Erzurum Alışveriş Merkezi No:43, Erzurum/Merkez" , RequiredCount=30 },
-            new ProductItemBase { Id = 5, DefterNo = 533532323, DepoGirisi = DateTime.Now, MusteriAd = "Armut Kırtasiye", UrunCins = "Kitap", UrunAdet = 3539531, UrunAgirlik = 210 , DepoKonum = "Saray Mh. İsmet Hilmi Balcı Cd.Damlataş Eğlence Merkezi Önü, Antalya/Merkez" , RequiredCount=40},
-            new ProductItemBase { Id = 6, DefterNo = 234322443, DepoGirisi = DateTime.Now, MusteriAd = "FarmTown", UrunCins = "Tahıl", UrunAdet = 3539531, UrunAgirlik = 650 , DepoKonum = "Karlıktepe Mah., Ak Sokak, No:4 Kars/Merkez" , RequiredCount=50 },
-            new ProductItemBase { Id = 7, DefterNo = 234324324, DepoGirisi = DateTime.Now, MusteriAd = "PetCat", UrunCins = "Hayvan Maması", UrunAdet = 3539531, UrunAgirlik = 50 , DepoKonum = "Yukarı Mumcu Çaykara Caddesi No:20 Erzurum/Oltu " , RequiredCount=60}
+            new ProductItemBase { Id = 1, DefterNo = 123321456, DepoGirisi = DateTime.Now, MusteriAd = "Beko", UrunCins = "Mobilya", UrunAdet = 7839858, UrunAgirlik = 195 , DepoKonum = "A blok", RequiredCount=5 },
+            new ProductItemBase { Id = 2, DefterNo = 213213123, DepoGirisi = DateTime.Now, MusteriAd = "Penta Teknoloji", UrunCins = "Ekran Kartı", UrunAdet = 3243243, UrunAgirlik = 250 , DepoKonum = "B blok",RequiredCount=10},
+            new ProductItemBase { Id = 3, DefterNo = 453223323, DepoGirisi = DateTime.Now, MusteriAd = "Vatan Computer", UrunCins = "MousePad", UrunAdet = 4829424, UrunAgirlik = 300 , DepoKonum = "C blok", RequiredCount=20},
+            new ProductItemBase { Id = 4, DefterNo = 234142323, DepoGirisi = DateTime.Now, MusteriAd = "Teknosa", UrunCins = "Laptop", UrunAdet = 2935643, UrunAgirlik = 500 , DepoKonum = "D blok" , RequiredCount=30 },
+            new ProductItemBase { Id = 5, DefterNo = 533532323, DepoGirisi = DateTime.Now, MusteriAd = "Armut Kırtasiye", UrunCins = "Kitap", UrunAdet = 3539531, UrunAgirlik = 210 , DepoKonum = "E blok" , RequiredCount=40},
+            new ProductItemBase { Id = 6, DefterNo = 234322443, DepoGirisi = DateTime.Now, MusteriAd = "FarmTown", UrunCins = "Tahıl", UrunAdet = 3539531, UrunAgirlik = 650 , DepoKonum = "F blok" , RequiredCount=50 },
+            new ProductItemBase { Id = 7, DefterNo = 234324324, DepoGirisi = DateTime.Now, MusteriAd = "PetCat", UrunCins = "Hayvan Maması", UrunAdet = 3539531, UrunAgirlik = 50 , DepoKonum = "G blok " , RequiredCount=60}
         };
 
     #endregion
@@ -18,11 +19,14 @@ public class ProductModel : BaseViewModel {
     #region Properties
 
     private int _count;
-    public int Count {
+    public int Count
+    {
         get { return _count; }
-        set {
+        set
+        {
 
-            if (_count != value) {
+            if (_count != value)
+            {
                 _count = value;
                 OnPropertyChanged(nameof(Count));
             }
@@ -35,10 +39,13 @@ public class ProductModel : BaseViewModel {
 
 
     private string selectedProduct;
-    public string SelectedProduct {
+    public string SelectedProduct
+    {
         get { return selectedProduct; }
-        set {
-            if (selectedProduct != value) {
+        set
+        {
+            if (selectedProduct != value)
+            {
                 selectedProduct = value;
                 OnPropertyChanged(nameof(SelectedProduct));
 
--- EasyBarcodeMAU/ProductListePage.xaml.cs
+++ EasyBarcodeMAU/ProductListePage.xaml.cs
@@ -1,53 +1,62 @@
 using EasyBarcodeMAU.Models;
 
 namespace EasyBarcodeMAU;
-    public partial class ProductListPage : ContentPage {
+public partial class ProductListPage : ContentPage
+{
 
-        #region Variables
-        private ProductItemBase selectedItem;
+    #region Variables
+    private ProductItemBase selectedItem;
     #endregion
 
-  
+
 
 
     #region InitModel
-    public ProductListPage() {
-            InitializeComponent();
-            BindingContext = new ProductModel();
-        }
-
-        #endregion
-
-        #region Methods
-
-        private async void Frame_Tapped(object sender, EventArgs e)
-{
-    if (sender is Frame tappedFrame)
+    public ProductListPage()
     {
-        if (tappedFrame.BindingContext is ProductItemBase itemBase)
+        InitializeComponent();
+        BindingContext = new ProductModel();
+    }
+
+    #endregion
+
+    #region Methods
+
+    private async void Frame_Tapped(object sender, EventArgs e)
+    {
+        if (sender is Frame tappedFrame)
         {
-            if (selectedItem != null)
+            if (tappedFrame.BindingContext is ProductItemBase itemBase)
             {
-                    selectedItem.BorderColor = Color.FromRgb(128, 128, 128);  
-                                                                            
+                if (selectedItem != null)
+                {
+                    selectedItem.BorderColor = Color.FromRgb(128, 128, 128);
                 }
 
-                var greenColor = Color.FromRgba(0, 255, 0, 255); 
-                tappedFrame.BorderColor = greenColor;   
+                var greenColor = Color.FromRgba(0, 255, 0, 255);
+                tappedFrame.BorderColor = greenColor;
 
-            selectedItem = itemBase;
+                selectedItem = itemBase;
 
-            var scanPage = new ScanBarcodeScreen(selectedItem);
-            await Navigation.PushAsync(scanPage);
+                var scanPage = new ScanBarcodeScreen(selectedItem);
+                await Navigation.PushAsync(scanPage);
+            }
+        }
+
+        // Eðer geri döndüðünüzde veya baþka bir Frame'e týkladýðýnýzda eski Frame'in rengini eski haline getirin.
+        if (selectedItem != null && selectedItem != (sender as Frame)?.BindingContext)
+        {
+            selectedItem.BorderColor = Color.FromRgb(128, 128, 128);
+            selectedItem = null;
         }
     }
-}
 
 
-        private void OnConfirmButtonClicked(object sender, EventArgs e) {
-            var scanPage = new ScanBarcodeScreen();
-            Navigation.PushAsync(scanPage);
-        }    
-#endregion
+    private void OnConfirmButtonClicked(object sender, EventArgs e)
+    {
+        var scanPage = new ScanBarcodeScreen();
+        Navigation.PushAsync(scanPage);
+    }
+    #endregion
 
 }
