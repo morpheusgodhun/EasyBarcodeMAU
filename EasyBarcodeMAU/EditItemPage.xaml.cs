ce1f6fafe0a2e570087f9f605f59995272d6dcaa
Orhun Yusuf Babaoglu <74192702+morpheusgodhun@users.noreply.github.com>
2023-10-17 11:30:04 +0300

    --

--- EasyBarcodeMAU/EditItemPage.xaml.cs
+++ EasyBarcodeMAU/EditItemPage.xaml.cs
@@ -1,40 +1,37 @@
 using System.Collections.ObjectModel;
-
 using EasyBarcodeMAU.Models;
 
-namespace EasyBarcodeMAU
-{
-    public partial class EditItemPage : ContentPage
-    {
+namespace EasyBarcodeMAU {
+    public partial class EditItemPage : ContentPage {
         private int _readedCount;
+        private int _count;
         private ProductItemBase _selectedItem;
         private ReadBaseModel viewModel;
         private ObservableCollection<ReadBaseModel> scannedBarcodes;
 
-        public EditItemPage(ProductItemBase selectedItem, int readedCount, ObservableCollection<ReadBaseModel> scannedBarcodes)
-        {
+        public EditItemPage(ProductItemBase selectedItem, int readedCount, ObservableCollection<ReadBaseModel> scannedBarcodes) {
             InitializeComponent();
             _selectedItem = selectedItem;
             _readedCount = readedCount;
             this.scannedBarcodes = scannedBarcodes;
             viewModel = new ReadBaseModel();
             viewModel.ReadedCount = _readedCount;
+            viewModel.Count = _count;
             BindingContext = viewModel;
         }
 
-        protected override void OnAppearing()
-        {
+        protected override void OnAppearing() {
             base.OnAppearing();
             barcodeListView.ItemsSource = scannedBarcodes;
         }
 
         private void ArtirButton_Clicked(object sender, EventArgs e) {
-            viewModel.Count++;
+            _count++;
         }
 
         private void AzaltButton_Clicked(object sender, EventArgs e) {
-            if (viewModel.Count > 0) {
-                viewModel.Count--;
+            if (_count > 0) {
+                _count--;
             }
         }
     }
