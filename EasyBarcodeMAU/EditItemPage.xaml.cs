80fd5d743a1da051f4c795ef7d1edb1f064bdf75
Orhun Yusuf Babaoglu <74192702+morpheusgodhun@users.noreply.github.com>
2023-10-18 10:10:48 +0300

    duzeltme

--- EasyBarcodeMAU/EditItemPage.xaml.cs
+++ EasyBarcodeMAU/EditItemPage.xaml.cs
@@ -2,34 +2,36 @@ using System.Collections.ObjectModel;
 using EasyBarcodeMAU.Models;
 
 namespace EasyBarcodeMAU;
-    public partial class EditItemPage : ContentPage {
-        private int _readedCount;
-        private int _count;
-        private ProductItemBase _selectedItem;
-        private ReadBaseModel viewModel;
-        private ObservableCollection<ReadBaseModel> scannedBarcodes;
+public partial class EditItemPage : ContentPage {
+    private int _readedCount;
+    private int _count;
+    private ProductItemBase _selectedItem;
+    private ReadBaseModel viewModel;
+    private ObservableCollection<ReadBaseModel> scannedBarcodes;
 
-        public EditItemPage(ProductItemBase selectedItem, int readedCount, ObservableCollection<ReadBaseModel> scannedBarcodes) {
-            InitializeComponent();
-            _selectedItem = selectedItem;
-            _readedCount = readedCount;
-            this.scannedBarcodes = scannedBarcodes;
-            viewModel = new ReadBaseModel();
-            viewModel.ReadedCount = _readedCount;
-            viewModel.Count = _count;
-            BindingContext = viewModel;
-        }
+    public EditItemPage(ProductItemBase selectedItem, int readedCount, ObservableCollection<ReadBaseModel> scannedBarcodes) {
+        InitializeComponent();
+        _selectedItem = selectedItem;
+        _readedCount = readedCount;
+        this.scannedBarcodes = scannedBarcodes;
+        viewModel = new ReadBaseModel();
+        viewModel.ReadedCount = _readedCount;
+        viewModel.Count = _count;
+        BindingContext = viewModel;
+    }
 
-        protected override void OnAppearing() {
-            base.OnAppearing();
-            barcodeListView.ItemsSource = scannedBarcodes;
-        }
+    protected override void OnAppearing() {
+        base.OnAppearing();
+        barcodeListView.ItemsSource = scannedBarcodes;
+    }
 
-        private void ArtirButton_Clicked() {
-            _count++;
-        }
+    private void ArtirButton_Clicked(object sender, EventArgs e) {
+        _count++;
+    }
 
-        private void AzaltButton_Clicked() {           
-             _count--;          
+    private void AzaltButton_Clicked(object sender, EventArgs e) {
+        if (_count > 0) {
+            _count--;
         }
     }
+}
